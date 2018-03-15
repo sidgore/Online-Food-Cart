707399ae79144253782f0096b2d650d71b702e8e
SIDDHARTH GORE <siddharthgore@SIDDHARTHs-MacBook-Air.local>
2018-03-15 03:33:53 -0700

    Removed unused imports

--- src/Footer.js
+++ src/Footer.js
@@ -1,1 +1,7 @@
 
+import React from 'react';
+const Footer = () => {
+
+    return (<div className='footer'> &copy; 2018 A Siddharth Gore<sup>&trade;</sup> Production</div>)
+}
+export default Footer;
